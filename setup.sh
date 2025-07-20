deb8b855b8cf5dddce0668e64012517227887bb7
hamza-Algohary <hamza.moza.mizo@gmail.com>
2025-07-20 17:05:57 +0300

    stuff

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+# sudo apt install $(grep -vE "^\s*#" apt-packages | tr "\n" " ")
+# flatpak install $(grep -vE "^\s*#" flatpaks | tr "\n" " ")
 
-sudo apt install $(grep -vE "^\s*#" apt-packages | tr "\n" " ")
-flatpak install $(grep -vE "^\s*#" flatpaks | tr "\n" " ")
-
-cp tvui.jar /usr/local/bin/tvui.jar
-chmod +x /usr/local/bin/tvui.jar
+sudo cp tvui.jar /usr/local/bin/tvui.jar
+sudo chmod +x /usr/local/bin/tvui.jar
 sudo cp tvui.desktop /etc/xdg/autostart/
 chmod 644 /etc/xdg/autostart/tvui.desktop
